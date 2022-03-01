29254cbe29533530c97cbe9ecbdc1e1d5a0fdb2d
Bekir Gulestan <gulestanbekir@gmail.com>
2022-03-01 21:10:32 +0300

    Comment Create Add
    Comment Create Component added

--- Client/src/PostList.js
+++ Client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import CommentCreate from './CommentCreate'
 
 const PostList = ()=>{
     
@@ -21,6 +22,7 @@ const PostList = ()=>{
         >
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentCreate postId={post.id}/>
             </div>
 
 
