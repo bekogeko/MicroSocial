6e160d9c068f5034ab96b12639ee7d107022b33b
Bekir Gulestan <gulestanbekir@gmail.com>
2022-03-06 03:01:09 +0300

    Client deployment config created

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,14 +3,15 @@ import PostCreate from "./PostCreate";
 import PostList from "./PostList";
 
 const App = () => {
-  return <div className="container">
-    <h1>Create a new Post</h1>
-    <PostCreate/>
-    <hr/>
-    <h1>Posts</h1>
-    <PostList/>
-
-  </div>;
+  return (
+    <div className="container">
+      <h1>Create a new Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
 };
- 
+
 export default App;
--- Client/src/PostCreate.js
+++ Client/src/PostCreate.js
@@ -1,35 +1,33 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
 
-const PostCreate = ()=>{
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-    const [title,setTitle] = useState('')
+    await axios.post("http://posts.com/posts", {
+      title,
+    });
+    setTitle("");
+  };
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
-
-        await axios.post('http://localhost:4000/posts',{
-            title
-        })
-        setTitle('')
-
-    }
-
-
-    return <div>
-        <form onSubmit={onSubmit} className="form-group">
-            <div className="">
-                <label>Title</label>
-                <input 
-                    value={title}
-                    onChange={e=>setTitle(e.target.value)}
-                    className="form-control"
-                />
-            </div>
-            <br/>
-            <button className="btn btn-primary" >Submit</button>
-        </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form-group">
+        <div className="">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <br />
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-}
+  );
+};
 export default PostCreate;
